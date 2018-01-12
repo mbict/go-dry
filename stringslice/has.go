4399ea599645f0a138e6207b1910ac50180f9d15
Michael Boke <michael.boke@gmail.com>
2018-01-12 15:56:16 +0100

    added case insensitive compare for stringslice.HasCI

--- stringslice/has.go
+++ stringslice/has.go
@@ -1,8 +1,20 @@
 package stringslice
 
+import "strings"
+
 func Has(slice []string, value string) bool {
 	for i := range slice {
 		if slice[i] == value {
+			return true
+		}
+	}
+	return false
+}
+
+// HasCI is case insensitve version of has
+func HasCI(slice []string, value string) bool {
+	for i := range slice {
+		if strings.EqualFold(slice[i], value) {
 			return true
 		}
 	}
