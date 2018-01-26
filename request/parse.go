f72d9f5cfe6f5fecf40c603d21121cdfa73b72de
Michael Boke <michael.boke@gmail.com>
2018-01-26 10:51:23 +0100

    added request parser for string and int slices

---
+++ request/parse.go
@@ -0,0 +1,53 @@
+package request
+
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+func ParseInts(v string) ([]int, error) {
+	v = strings.TrimSpace(v)
+	if len(v) == 0 {
+		return nil, nil
+	}
+
+	var err error
+	rs := strings.Split(v, ",")
+	ints := make([]int, len(rs))
+	for i, val := range rs {
+		ints[i], err = strconv.Atoi(val)
+		if err != nil {
+			return nil, fmt.Errorf("has a invalid integer value `%s`", val)
+		}
+	}
+	return ints, nil
+}
+
+func ParseStrings(v string) ([]string, error) {
+	v = strings.TrimSpace(v)
+	if len(v) == 0 {
+		return nil, nil
+	}
+
+	strs := strings.Split(v, ",")
+	for i := len(strs) - 1; i >= 0; i-- {
+		str, err := url.QueryUnescape(strs[i])
+		if err != nil {
+			return nil, fmt.Errorf("has a invalid url encoded value `%s`", strs[i])
+		}
+		str = strings.TrimSpace(str)
+
+		//remove empty elements
+		if len(str) == 0 {
+			strs = append(strs[:i], strs[i+1:]...)
+			continue
+		}
+		strs[i] = str
+	}
+	if len(strs) == 0 {
+		return nil, nil
+	}
+	return strs, nil
+}
